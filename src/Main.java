610a1e800d04fb33fde0bd715b4127499be5e690
Aurora Scalici <aurorascalici1899@icloud.com>
2024-02-23 17:14:57 +0100

    create a Main

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/*Scrivere un programma che contenga una interfaccia Forma 
+ed un metodo chiamato calcolaArea().Crea poi due sottoclassi 
+Rettangolo e Triangolo che implementano Forma ed implementano 
+il metodo per il calcolo dell'area.*/
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
